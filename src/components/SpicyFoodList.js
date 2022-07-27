1ec60e35a92e3e1e51e9cc284897dd2bdc88b582
jacksonohallo <jacksonohallo@gmail.com>
2022-07-27 20:02:49 +0300

    done

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -26,13 +26,37 @@ function handleLiClick(id) {
 }
   const foodList = foods.map((food) => (
     
+    
     <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+  const [filterBy, setFilterBy] = useState("All");
+  function handleFilterChange(event) {
+  setFilterBy(event.target.value);
+  }
+
+
+const foodsToDisplay = foods.filter((food) => {
+  if (filterBy === "All") {
+    return true;
+  } else {
+    return food.cuisine === filterBy;
+  }
+});
+
 
   return (
     <div>
+
+<select name="filter" onChange={handleFilterChange}                         >
+  <option value="All">All</option>
+  <option value="American">American</option>
+  <option value="Sichuan">Sichuan</option>
+  <option value="Thai">Thai</option>
+  <option value="Mexican">Mexican</option>
+</select>
+
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
